62c956fb6d683888372455429c4e4e7784cd18c9
dmitry <dmitry.metelitsa@gmail.com>
2022-06-24 07:15:55 +0300

    screen2.dart + textfield.dart

---
+++ lib/ui/screens/screen2/widgets/textfield.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class TextFieldWidget extends StatelessWidget {
+  const TextFieldWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
